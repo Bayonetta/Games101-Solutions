8c26fda0b10fe0f48876df0abaddc3636717fd46
Bayonetta <shuaizhao0315@outlook.com>
2023-05-04 17:04:40 +0800

    Update Object.hpp

--- Assignment5/codes/Object.hpp
+++ Assignment5/codes/Object.hpp
@@ -1,3 +1,25 @@
+/*
+这是一个名为Object的类，作为光线追踪中物体的基类，包含一些虚函数和一些成员变量，用于描述物体的属性和行为。
+
+类的成员变量包括：
+
+- materialType：材质类型，包括DIFFUSE（漫反射）, MIRROR（镜面反射）, GLASS(折射/透明)等。
+- ior：折射率（即折射光线的速度相对于真空中的速度）。
+- Kd：漫反射系数（浅色的物体可能更具散射性，而不具有镜面反射的能力）。
+- Ks：镜面反射系数（在物体表面上具有光泽的区域）。
+- diffuseColor：物体的漫反射颜色（通过将光线散射到周围来混合反射光）。
+- specularExponent：物体表面上的高光反射效果的强度。
+
+该类还包含以下纯虚函数：
+
+- bool intersect(const Vector3f&, const Vector3f&, float&, uint32_t&, Vector2f&) const：检查该对象是否与射线相交。
+- void getSurfaceProperties(const Vector3f&, const Vector3f&, const uint32_t&, const Vector2f&, Vector3f&, Vector2f&) const：返回给定点表面的颜色和法向量。
+
+该类还有一个虚函数：
+
+- Vector3f evalDiffuseColor(const Vector2f&) const：返回物体的漫反射颜色。
+*/
+
 #pragma once
 
 #include "Vector.hpp"
