e0235c0334943431df6d7cd3c611dc87e2c3270f
Bayonetta <shuaizhao0315@outlook.com>
2023-05-07 16:04:11 +0800

    Update Renderer.cpp

--- Assignment5/codes/Renderer.cpp
+++ Assignment5/codes/Renderer.cpp
@@ -229,10 +229,16 @@ void Renderer::Render(const Scene& scene)
             // vector that passes through it.
             // Also, don't forget to multiply both of them with the variable *scale*, and
             // x (horizontal) variable with the *imageAspectRatio*            
-            x = 2 * scale * imageAspectRatio / scene.width * (i + 0.5) - scale * imageAspectRatio;
-            y = - 2 * scale / scene.height * (j + 0.5) + scale;
-            Vector3f dir = Vector3f(x, y, -1); // Don't forget to normalize this direction!
-            dir = normalize(dir);
+            float x_proj = 2 * (i + 0.5) / (float)scene.width - 1;
+            float y_proj = 1 - 2 * (j + 0.5) / (float)scene.height;
+            
+            float x_view = imageAspectRatio * scale * x_proj;
+            float y_view = scale * y_proj;
+            
+			x = x_view;
+			y = y_view;
+
+            Vector3f dir = normalize(Vector3f(x, y, -1));
             framebuffer[m++] = castRay(eye_pos, dir, scene, 0);
         }
         UpdateProgress(j / (float)scene.height);
